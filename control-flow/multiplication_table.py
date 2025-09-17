2de47459df9be569f46f4d42e358226435d752ee
ebubeikechi <ebubeikechi@gmail.com>
2025-09-17 19:23:02 +0100

    removing the step from the for range loop

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,7 +2,7 @@ def main():
 
     number = int(input("Enter a number to see its multiplication table: "))
 
-    for i in range(1,11,1):
+    for i in range(1,11):
         print(f"{number} * {i} = {number * i}")
 
     return
