90c7de40c836d1045a8dd4878cb07a35fc5a691f
ebubeikechi <ebubeikechi@gmail.com>
2025-09-24 15:29:52 +0100

    arithmetic_operations file

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,18 @@
+def perform_operation(num1, num2, operation):
+
+    if type(num1) == float and type(num2) == float:
+        match operation:
+            case 'add':
+                return num1 + num2
+            case 'subtract':
+                return num1 - num2
+            case 'multiply':
+                return num1 * num2
+            case 'divide':
+                if num2 == 0:
+                    return "cannot divide by zero !"
+                return num1 / num2
+    else:
+        print("numbers are not float")
+
+    return
