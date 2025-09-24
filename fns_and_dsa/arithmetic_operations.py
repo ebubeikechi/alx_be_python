8723baaf6dc39c8973b13112813a43654e5e81dd
ebubeikechi <ebubeikechi@gmail.com>
2025-09-24 15:32:09 +0100

    inclusion of elif in arithmetic file

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -11,7 +11,8 @@ def perform_operation(num1, num2, operation):
             case 'divide':
                 if num2 == 0:
                     return "cannot divide by zero !"
-                return num1 / num2
+                elif num2 != 0:
+                    return num1 / num2
     else:
         print("numbers are not float")
 
