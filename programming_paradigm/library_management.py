014830dd3c9a915b8058995c8bbef8682646fd92
ebubeikechi <ebubeikechi@gmail.com>
2025-10-04 20:44:02 +0100

    corrected books list name

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -17,29 +17,29 @@ class Book:
 
 class Library:
     def __init__(self):
-        self.list_books = []
+        self._books = []
 
     def add_book(self, book):
-        self.list_books.append(book)
+        self._books.append(book)
 
     def check_out_book(self, title):
-        for book in self.list_books:
+        for book in self._books:
             if book.title == title and not book.is_checked_out():
                 book.check_out()
                 
 
     def return_book(self, title):
-        for book in self.list_books:
+        for book in self._books:
             if book.title == title and book.is_checked_out():
                 book.return_book()
 
 
     def list_available_books(self):
 
-        for book in self.list_books:
+        for book in self._books:
             if not book.is_checked_out():
                 print(f"{book.title} by {book.author}")
-        #print([book.title for book in self.list_books if not book.is_checked_out()])
+        #print([book.title for book in self._books if not book.is_checked_out()])
 
 
     
