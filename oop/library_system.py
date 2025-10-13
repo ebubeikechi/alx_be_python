408f8e387630bc046558699f928fcf4a040ad75b
ebubeikechi <ebubeikechi@gmail.com>
2025-10-13 22:29:24 +0100

    oop task 1 files

---
+++ oop/library_system.py
@@ -0,0 +1,55 @@
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def list_books(self):
+        for book in self.books:
+            print(f"{book}: {book.title} by {book.author}")
+
+
+class Book:
+    def __init__(self, title: str, author: str):
+        self.title = title
+        self.author = author
+
+    def __repr__(self):
+        return "Book"
+
+
+class EBook(Book):
+    def __init__(self, title: str, author: str, file_size: int):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+    def __repr__(self):
+        return "EBook"
+
+
+class PrintBook(Book):
+    def __init__(self, title: str, author: str, page_count: int):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def __repr__(self):
+        return "PrintBook"
+
+
+# def main():
+#     my_library = Library()
+
+#     classic_book = Book("Pride and Prejudice", "Jane Austen")
+#     digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
+#     paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
+
+#     my_library.add_book(classic_book)
+#     my_library.add_book(digital_novel)
+#     my_library.add_book(paper_novel)
+
+#     my_library.list_books()
+
+
+# if __name__ == "__main__":
+#     main()
